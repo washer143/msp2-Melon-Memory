562ba25af9043c1fe4760c9be0f5c566d573d92e
ryan washer <whitewater143@hotmail.com>
2023-08-09 21:40:04 +0000

    add buttons and styling

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let flippedCard = false;
+let firstCard, secondCard;
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+function flipCard (){
+    this.classlist.add('flip');
+    if (!flippedCard){
+        flippedCard = true;
+        firstCard = this;
+    } else {
+        flippedCard = false;
+        secondCard = this; return;
+ 
+    }
+
+}
