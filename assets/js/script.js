47f62cd04532c1198c972403417b6857d2eb53b1
ryan washer <whitewater143@hotmail.com>
2023-08-17 20:05:12 +0000

    add shuffle, resetboard reset board function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,61 @@
 const cards = document.querySelectorAll('.memory-card');
 
-let flippedCard = false;
+
+let FlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-function flipCard (){
-    this.classList.toggle('flip');
-    if (!flippedCard){
-        flippedCard = true;
-        firstCard = this;
-    } else {
-        flippedCard = false;
-        secondCard = this; return;
- 
-    }
+  this.classList.add('flip');
 
+  if (!FlippedCard) {
+    FlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
 }
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard() {
+  [FlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 16);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
