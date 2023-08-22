dd990f27986259f21df956c42ccee8cc00e9b91e
ryan washer <whitewater143@hotmail.com>
2023-08-22 13:25:35 +0000

    add function to game won to show winning scores and add reset function to activate reset button to restart game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
 const cards = document.querySelectorAll('.memory-card');
 const htpModal = document.getElementById('htp-modal');
-const winModal = document.getElementById('modal');
-//const leaderBoard = document.getElementsById('#leaderboard-btn');
-const moveContainer = document.getElementById('moves');
-const timerContainer = document.getElementById('timer');
+const modal = document.querySelector('.modal');
+const moveContainer = document.querySelector('#moves');
+const timerContainer = document.querySelector('#timer');
 const modalBtn = document.getElementById('modal-btn');
 const modalClose = document.querySelector('.modal-close');
-const allPairsMatched = 8;
+const storageContainer = document.querySelector('.storage');
+const info = document.querySelector('.info');
+const saveBtn = document.querySelector('.save-btn');
+const storedInput = localStorage.getItem('text-input');
+const AllPairsMatched = 8;
 
 
-let FlippedCard = false;
+let flippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 let moves = 0;
@@ -23,17 +26,19 @@ let timeStart = false;
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
+
 modalBtn.addEventListener('click', showHtpModal); 
-modalClose.addEventListener('click', closeHtpModal); 
+modalClose.addEventListener('click', closeHtpModal);
+
  
- 
- function showHtpModal() {
+ function showHtpModal(){
      htpModal.style.display = "block";
  }
- 
- function closeHtpModal() {
+
+ function closeHtpModal(){
      htpModal.style.display = "none";
- }
+}
+ 
 
 function flipCard() {
   if (!gameStart) {
@@ -43,8 +48,8 @@ function flipCard() {
 
   this.classList.add('flip');
 
-  if (!FlippedCard) {
-    FlippedCard = true;
+  if (!flippedCard) {
+    flippedCard = true;
     firstCard = this;
 
     return;
@@ -61,7 +66,7 @@ function checkForMatch() {
   if (isMatch) pairMatch();
     else noMatch();
 
-  if (matchedPair === allPairsMatched) gameWon();
+  if (matchedPair === AllPairsMatched) gameWon();
 
 }
 
@@ -94,7 +99,7 @@ function noMatch (){
      moveContainer.innerHTML = moves;
  }
 
- timerContainer.innerHTML = "Clock " + minutes + " : " + seconds;
+ timerContainer.innerHTML = "Time " + minutes + " : " + seconds;
  
  function timer() {
      clock = setInterval(function() {
@@ -103,18 +108,17 @@ function noMatch (){
              minutes++;
              seconds = 0;
          }
-         timerContainer.innerHTML = "Clock " + minutes + " : " + seconds;
+         timerContainer.innerHTML = "Time " + minutes + " : " + seconds;
      }, 1000);
  }
  
  function stopTime() {
-     clearInterval(Clock);
+     clearInterval(clock);
  }
 
 
-
 function resetBoard() {
-  [FlippedCard, lockBoard] = [false, false];
+  [flippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
 
@@ -123,12 +127,47 @@ function gameWon() {
   showWinModal();
 }
 
+function showWinModal() {
+  modal.style.display = "block";
+  timeResult = timerContainer.innerHTML;
+  document.getElementById("total-moves").innerHTML = moves;
+  document.getElementById("total-time").innerHTML = timeResult;
+  reset();
+}
 
 
-(function shuffle() {
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 16);
-    card.style.order = randomPos;
+window.onclick = function(event) {
+  if (event.target.classList == 'modal-close') {
+      document.getElementById('modal').style.display = "none";
+  }
+};
+
+
+function shuffle() {
+  cards.forEach(cards => {
+    let random = Math.floor(Math.random() * 16);
+    cards.style.order = random;
   });
-})();
+};
+
+
+function reset() {
+    
+  setTimeout(() => {
+    stopTime();
+    shuffle();
+    flippedCard = false;
+    [firstCard, secondCard] = [null, null];
+    gameStart = false;
+    timeStart = false;
+    seconds = 0;
+    minutes = 0;
+    moves = 0;
+    matchedPair = 0;
+    timerContainer.innerHTML = "Timer 0:00";
+    moveContainer.innerHTML = 0;
+    cards.forEach(cardReset => cardReset.classList.remove('flip'))
+    cards.forEach(card => card.addEventListener('click', flipCard));
+  }, 500);
+}
 
