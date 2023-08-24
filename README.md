351699f9296611ae9058f2bc745f6153a7d3edba
ryan washer <whitewater143@hotmail.com>
2023-08-24 01:07:39 +0000

    add all image documentation, and adding to read.me file

--- README.md
+++ README.md
@@ -2,33 +2,63 @@
  ![responsive-media](screenshots/am-i-responsive.png)
 
 # Introduction
-This project is a classic memory game, where you click one card to reveal a character and you must choose another card to reveal the same character, if its not the same character you must restart and try to remember where those characters are to match the same two characters. When two characters are a match they will stay is position until all characters have been paired, with the final amount pairs being 8, revealing a time and the amount of moves it took to complete the game.
+This project is a classic memory game with a watermelon theme, where you click one card to reveal a character and you must choose another card to reveal the same character, if its not the same character you must restart and try to remember where those characters are to match the same two characters. When two characters are a match they will stay is position until all characters have been paired, with the final amount pairs being 8, revealing a time and the amount of moves it took to complete the game.
 
 # Design
-The goal of the game was to create and appealing and fun interactive game that the user can restart and play as many times as they wish. The game was designed with young users in mind targeting young children to adolescents, an emphasis on how fast and how many little moves you can do it in was used target the older age range, adding a competitive element to game.
+The goal of the game was to create and appealing and fun interactive game that the user can restart and play as many times as they wish. The game was designed with young users in mind targeting young children to adolescents, an emphasis on how fast and how many little moves you can do it in was used target the older age range, adding a competitive element to game. The timer counts up for as long is necessary so younger players don't have the added pressure of finishing the game under a set time. 
 
-# User Experience (ux)
+## Colors 
+The color palette i chose for this project was obviously going to be based around watermelon colors, with an emphasis on contrast and brightness to intrigue a younger user to play the game. This was my initial color scheme.
+
+![color-palette](screenshots/color-palette.png)
+
+## Fonts
+The font chosen for the game was essential to the look and feel of the game, whilst searching i came across Rammetto One, i feel this font gave the game a very playful feel and would be attractive to a younger audience, with that in mind i made use of it throughout the game, to maintain the identity of the game. 
 
 ## User goals:
 # Features
 ## Exisiting Features
 ## Future Features
-## Colors 
+
+
 
 
 ## Fonts
 # Manual Testing
+# Lighthouse Testing
+I used google chrome in incognito mode for lighthouse testing, i formatted all pictures to webp format to reduce loading time.
+
+## Mobile Test.
+<img src="assets/documentation/lighthouse/mobile-lighthouse-score.png" width=50% height=50%>
+
+## Desktop Test.
+<img src="assets/documentation/lighthouse/desktop-lighthouse-score.png" width=50% height=50%> 
 
 # Testing User Stories
-# Validation
+# Validator Testing
+
+## Css Validator
+
+<img src="assets/documentation/validation/css-validator.png" width=50% height=50%> 
+
+## Html validator
+<img src="assets/documentation/validation/html-validator.png" width=50% height=50%> 
+
+## Jshint validator
+<img src="assets/documentation/validation/jshint-validation.png" width=50% height=50%> 
+
+### All validation tests passed with no errors being found.
 
 # Browser Compatibility
 The website was tested on the following browsers:
-* Google Chrome
-* Safari
-* Mozilla Firefox
 
-They all appeared as they should.
+|Browser  |Main Screenshot  |How To Play Screenshot    |Win Modal Screenshot   |Assessment   |   
+|---      |---                                                           |---|---|---|
+| Chrome  | <img src="screenshots/chrome.png"> | <img src="screenshots/chrome-htp.png"> | <img src="screenshots/chrome-wm.png">  |  Some slight differential between modal positions |
+| Microsoft Edge  | <img src="screenshots/microsoft-edge.png">  | <img src="screenshots/microsoft-edge-htp.png">    |  <img src="screenshots/microsoft-edge-wm.png">   |  Works as expected   |
+| Firefox  | <img src="screenshots/firefox.png">    | <img src="screenshots/firefox-htp.png">   | <img src="screenshots/firefox-wm.png">   |  Works as expected   | 
+| Opera  | <img src="screenshots/opera.png">  | <img src="screenshots/opera-htp.png">| <img src="screenshots/opera-wm.png">   |  Works as expected   |
+| Safari  | <img src="screenshots/safari.png">   | <img src="screenshots/safari-htp.png">   | <img src="screenshots/safari-wm.png">   | Works as expected  |
 
 # Deployment
 The website was deployed using github, these are the steps i followed.
@@ -51,7 +81,15 @@ If you wish to clone the repository these are the steps to take:
 
 If you want to fork the repository you can click the fork button located towards the top right of the page.
 
-# Responsiveness
+# Responsive Design
+|Device  | Screenshot  | Screenshot    | Screenshot   | Assessment  |   
+|---      |---|---|---|---| 
+|Galaxy fold  | <img src="screenshots/glaxy-fold-main.png" width="92%">  |  <img src="screenshots/galaxy-fold-htp.png">  |  <img src="screenshots/galaxy-fold-wm.png" width="96%">>  |   |   
+|Ipad air  | <img src="screenshots/ipad-air.png" width="93%"> >   |  <img src="screenshots/ipad-air-htp.png">  |  <img src="screenshots/ipad-air-wm.png" >  |   |   
+| Nesthub max | <img src="screenshots/nesthub-max.png" width="93%">>  | <img src="screenshots/nesthub-max-htp.png">   | <img src="screenshots/nesthub-max-wm.png">   |   
+|Samsung galaxy s8 | <img src="screenshots/samsung-galaxy-s8.png" width="94%">>   | <img src="screenshots/samsung-galaxy-s8-htp.png">   | <img src="screenshots/samsung-galaxy-s8-wm.png" >   |   |   
+|Surface pro  | <img src="screenshots/surface-pro-7.png" width="93%">   | <img src="screenshots/surface-pro-7-htp.png">   | <img src="screenshots/surface-pro-7-wm.png">   |   |   
+|Desktop dev/tools  | <img src="screenshots/desktop-main.png">   | <img src="screenshots/desktop-htp.png">   | <img src="screenshots/desktop-wm.png">   |   |   
 
 # Bugs
 
@@ -103,3 +141,6 @@ I'd like to thank my mentor Mitko Bacharov for guiding me in the right direction
 
 
    
+
+
+[def]: screenshots/color-palette.png
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,172 +6,144 @@ const timerContainer = document.querySelector('#timer');
 const modalBtn = document.getElementById('modal-btn');
 const modalClose = document.querySelector('.modal-close');
 const AllPairsMatched = 8;
-
 let flippedCard = false;
-let lockBoard = false; 
+let lockBoard = false;
 let firstCard, secondCard;
 let moves = 0;
 let matchedPair = 0;
 let timeResult = "";
 let gameStart = false;
+let clock;
 /* timer variables */
 let minutes = 0;
 let seconds = 0;
 let timeStart = false;
-
 /* listens for click to flip cards */
 cards.forEach(card => card.addEventListener('click', flipCard));
-
 /* event listener to listen for click on how to play modal */
-modalBtn.addEventListener('click', showHtpModal); 
+modalBtn.addEventListener('click', showHtpModal);
 modalClose.addEventListener('click', closeHtpModal);
 
- 
- function showHtpModal(){
-     htpModal.style.display = "block";
- }
-
- function closeHtpModal(){
-     htpModal.style.display = "none";
+function showHtpModal() {
+	htpModal.style.display = "block";
 }
- 
+
+function closeHtpModal() {
+	htpModal.style.display = "none";
+}
 /* This adds a flip function to the cards, adapted from
 https://www.youtube.com/watch?v=ZniVgo8U7ek FreeCodeCamp.org */
-
 function flipCard() {
-  if (!gameStart) {
-       gameStart = true;
-       timer();
+	if(!gameStart) {
+		gameStart = true;
+		timer();
+	}
+	this.classList.add('flip');
+	if(!flippedCard) {
+		flippedCard = true;
+		firstCard = this;
+		return;
+	}
+	secondCard = this;
+	checkForMatch();
 }
-
-  this.classList.add('flip');
-
-  if (!flippedCard) {
-    flippedCard = true;
-    firstCard = this;
-
-    return;
-  }
-
-  secondCard = this;
-  checkForMatch();
-}
-
 /* This function will check if cards match, code adapted from 
 https://www.youtube.com/watch?v=ZniVgo8U7ek FreeCodeCamp.org */
-
- function checkForMatch() {
-  let isMatch = firstCard.dataset.image === secondCard.dataset.image;
-  if (isMatch) matchedPair +=1;
-
-  if (isMatch) pairMatch();
-    else noMatch();
-
-  if (matchedPair === AllPairsMatched) gameWon();
-
+function checkForMatch() {
+	let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+	if(isMatch) matchedPair += 1;
+	if(isMatch) pairMatch();
+	else noMatch();
+	if(matchedPair === AllPairsMatched) gameWon();
 }
- /* This function will disable matched pairs */
-function pairMatch (){
-
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-
-  resetBoard();
+/* This function will disable matched pairs */
+function pairMatch() {
+	firstCard.removeEventListener('click', flipCard);
+	secondCard.removeEventListener('click', flipCard);
+	resetBoard();
 }
-
 /* This function will lock the board until un-matched cards are flipped back */
-function noMatch (){
-  lockBoard = true;
-
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-
-    resetBoard();
-  }, 1000);
-
-  plusMove();
+function noMatch() {
+	lockBoard = true;
+	setTimeout(() => {
+		firstCard.classList.remove('flip');
+		secondCard.classList.remove('flip');
+		resetBoard();
+	}, 1000);
+	plusMove();
 }
+moves = 0;
+moveContainer.innerHtml = 0;
 
- moves = 0;
- moveContainer.innerHtml = 0;
- 
- function plusMove() {
-     moves++;
-     moveContainer.innerHTML = moves;
- }
+function plusMove() {
+	moves++;
+	moveContainer.innerHTML = moves;
+}
+/* This function will activate the timer once the game is started */
+timerContainer.innerHTML = "Time " + minutes + " : " + seconds;
 
- /* This function will activate the timer once the game is started */
-
- timerContainer.innerHTML = "Time " + minutes + " : " + seconds;
- 
- function timer() {
-    let clock = setInterval(function() {
-         seconds++;
-         if (seconds === 59) {
-             minutes++;
-             seconds = 0;
-         }
-         timerContainer.innerHTML = "Time " + minutes + " : " + seconds;
-     }, 1000);
- }
- /* This function will clear the timer and will be called once the game is won */
- function stopTime() {
-     clearInterval(clock);
- }
-
+function timer() {
+	clock = setInterval(function() {
+		seconds++;
+		if(seconds === 59) {
+			minutes++;
+			seconds = 0;
+		}
+		timerContainer.innerHTML = "Time " + minutes + " : " + seconds;
+	}, 1000);
+}
+/* This function will clear the timer and will be called once the game is won */
+function stopTime() {
+	clearInterval(clock);
+}
 /* This function will reset the board after each round */
 function resetBoard() {
-  [flippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+	[flippedCard, lockBoard] = [false, false];
+	[firstCard, secondCard] = [null, null];
 }
 /* Calls game won function to initiate win modal pop up*/
 function gameWon() {
-  stopTime();
-  showWinModal();
+	stopTime();
+	showWinModal();
 }
 /* This function will activate win modal to pop up once game is complete */
 function showWinModal() {
-  modal.style.display = "block";
-  timeResult = timerContainer.innerHTML;
-  document.getElementById("total-moves").innerHTML = moves;
-  document.getElementById("total-time").innerHTML = timeResult;
-  reset();
+	modal.style.display = "block";
+	timeResult = timerContainer.innerHTML;
+	document.getElementById("total-moves").innerHTML = moves;
+	document.getElementById("total-time").innerHTML = timeResult;
+	reset();
 }
-
 /* this function will activate the close button when the win modal pops-up */
 window.onclick = function(event) {
-  if (event.target.classList == 'modal-close') {
-      document.getElementById('modal').style.display = "none";
-  }
+	if(event.target.classList == 'modal-close') {
+		document.getElementById('modal').style.display = "none";
+	}
 };
-
 /* This function will randomize card placement once game is started or reset code adapted from 
 https://www.youtube.com/watch?v=ZniVgo8U7ek FreeCodeCamp.org */
 function shuffle() {
-  cards.forEach(cards => {
-    let randomPosition = Math.floor(Math.random() * 16);
-    cards.style.order = randomPosition;
-  });
+	cards.forEach(cards => {
+		let randomPosition = Math.floor(Math.random() * 16);
+		cards.style.order = randomPosition;
+	});
 }
-
 /* This will activate reset button to restart the game */
 function reset() {
-    setTimeout(() => {
-    stopTime();
-    shuffle();
-    flippedCard = false;
-    [firstCard, secondCard] = [null, null];
-    gameStart = false;
-    timeStart = false;
-    seconds = 0;
-    minutes = 0;
-    moves = 0;
-    matchedPair = 0;
-    timerContainer.innerHTML = "Timer 0:00";
-    moveContainer.innerHTML = 0;
-    cards.forEach(cardReset => cardReset.classList.remove('flip'))
-    cards.forEach(card => card.addEventListener('click', flipCard));
-  }, 500);
+	setTimeout(() => {
+		stopTime();
+		shuffle();
+		flippedCard = false;
+		[firstCard, secondCard] = [null, null];
+		gameStart = false;
+		timeStart = false;
+		seconds = 0;
+		minutes = 0;
+		moves = 0;
+		matchedPair = 0;
+		timerContainer.innerHTML = "Timer 0:00";
+		moveContainer.innerHTML = 0;
+		cards.forEach(cardReset => cardReset.classList.remove('flip'));
+		cards.forEach(card => card.addEventListener('click', flipCard));
+	}, 500);
 }
-
